31f371573e5223a9121ac81a0af8b3f7ad51967a
Metzly <132518143+Metzly@users.noreply.github.com>
2025-01-06 11:58:58 -0400

    calendar

--- components/atoms/calendarComponent.tsx
+++ components/atoms/calendarComponent.tsx
@@ -8,19 +8,29 @@ const sheetId = process.env.NEXT_PUBLIC_GOOGLE_SHEET_ID;
 const apiKey = process.env.NEXT_PUBLIC_GOOGLE_API_KEY;
 const range = 'Sheet1!A:C'; 
 
-const fetchEvents = async () => {
+interface Event {
+  title: string;
+  start: Date;
+  end: Date;
+}
+
+const fetchEvents = async (): Promise<Event[]> => {
   const url = `https://sheets.googleapis.com/v4/spreadsheets/${sheetId}/values/${range}?key=${apiKey}`;
   const response = await fetch(url);
   const data = await response.json();
 
   if (!data.values) return [];
-
-  return data.values.slice(1).map(([title, start, end]) => ({
-    title,
-    start: new Date(start.split("/").reverse().join("-")),
-    end: new Date(end.split("/").reverse().join("-")),
-  }));
+  console.log(data.values);
+  return data.values.slice(1).map((item: [string, string, string]): Event => {
+    const [title, start, end] = item;
+    return {
+      title,
+      start: new Date(start.split("/").reverse().join("-")),
+      end: new Date(end.split("/").reverse().join("-")),
+    };
+  });
 };
+
 
 export default function CalendarPage() {
   const localizer = momentLocalizer(moment);
